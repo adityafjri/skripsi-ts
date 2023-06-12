775e863fc5cb6e936c8fd2f2ee7449b637b3a50c
adityafjri <adityafjri@gmail.com>
2023-06-12 15:01:40 +0700

    #

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 
-const prisma = new PrismaClient();
 const jsonData = JSON.parse(fs.readFileSync('./public/data/graphNode.json', 'utf8'));
 
 async function seed() {
   try {
-    await prisma.ListUndang.createMany({
+    await prisma.listUndang.createMany({
       data: jsonData.map(item => ({
         judul: item.id,
         tentang: item.tentang,
