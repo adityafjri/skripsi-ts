71b30df1e1ede1a3cc208cd4b782d202e9d208f7
adityafjri <adityafjri@gmail.com>
2023-06-12 14:39:32 +0700

    #

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Navigation from "../components/Navigation";
 import Hero from "../components/Hero";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
-import { getFeatures } from "./api/index";
 
 type Feature = {
   id: number;
@@ -22,9 +21,7 @@ const Home: React.FC<HomeProps> = ({ features }) => {
       <Hero />
       <section className="w-fit mx-auto flex flex-col items-center my-8">
         <div className="grid grid-cols-3 gap-6">
-            {features.map((feature) => (
-                <Card key={`fitur-${feature.id}`} feature={feature} />
-            ))}
+            
         </div>
       </section>
       <Footer />
@@ -33,8 +30,3 @@ const Home: React.FC<HomeProps> = ({ features }) => {
 };
 
 export default Home;
-
-export async function getStaticProps() {
-  const features: Feature[] = await getFeatures();
-  return { props: { features } };
-}
